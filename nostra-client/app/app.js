bb405e1da324a0ec729086305d4d79cd8d3d860d
Krzysztof Lis <k.lis@outlook.com>
2016-07-31 17:26:42 +0200

    Updated frontend build proccess, added angular-material & angular-google-charts

--- nostra-client/app/app.js
+++ nostra-client/app/app.js
@@ -1,3 +1,27 @@
-var angular = require('angular');
+import angular from 'angular';
+import { HomeCtrl } from './home/HomeCtrl';
 
-angular.module('nostra', []);
+require('angular-google-chart');
+require('angular-animate');
+require('angular-aria');
+require('angular-messages');
+require('angular-material');
+
+angular.module('nostra', [
+    require('angular-ui-router'),
+    require('angular-resource'),
+    'ngMaterial',
+    'googlechart'
+])
+    .config(($stateProvider, $urlRouterProvider) => {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: './app/home/home.tpl.html',
+                controller: 'HomeCtrl as home'
+            });
+
+        $urlRouterProvider.otherwise('/');
+    })
+    .controller('HomeCtrl', HomeCtrl)
+;
---
+++ nostra-client/app/home/HomeCtrl.js
@@ -0,0 +1,29 @@
+export class HomeCtrl {
+    constructor () {
+        this.chart = {
+            type: 'GeoChart',
+            data: [
+                ['State', 'Popularity'],
+                ['Texas', 200],
+                ['New York', 300],
+                ['US-IA', 20],
+                ['US-RI', 150]
+            ],
+            options: {
+                region: 'US',
+                resolution: 'provinces'
+            }
+        };
+        this.currentNavItem = 'home';
+    }
+
+    selectRegion(region) {
+        if (region) {
+            console.log(this.chart.data[region.row + 1]);
+            this.lastSelectedRegionIndex = region.row;
+        } else {
+            console.log(this.chart.data[this.lastSelectedRegionIndex + 1]);
+        }
+    }
+
+}
